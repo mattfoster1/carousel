58be951ded1e5edf8663418dd39e1b16198d6e0c
M Foster <matt@mfoster.co.uk>
2016-09-12 10:23:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// var n;
+
+// var rad = document.getElementsByName("slides1");
+// console.dir(rad);
+
+// for (var n=0; n < 3; n++) {
+// 	console.log(rad[0]);
+// 	rad.onclick = function() {
+// 	console.log("whoop");
+// 	}
+// }
+
+// console.dir(rad);
+
+var wrap = document.getElementsByClassName("wrap");
+var slide1 = document.getElementById("slide-1-trigger");
+console.log(wrap[0]);
+if (slide1.checked == true) {
+	wrap[0].style.left = "50px";
+} else if (document.getElementById("slide-1-trigger").checked == true) {
+} else if (document.getElementById("slide-1-trigger").checked == true) {
+} else if (document.getElementById("slide-1-trigger").checked == true) {
+}
