7e08e05e5a5253cd8c7dc1dcfbe39a4ea6035959
M Foster <matt@mfoster.co.uk>
2016-09-12 11:51:31 +0100

    second commit

--- script.js
+++ script.js
@@ -12,12 +12,18 @@
 
 // console.dir(rad);
 
-var wrap = document.getElementsByClassName("wrap");
-var slide1 = document.getElementById("slide-1-trigger");
-console.log(wrap[0]);
-if (slide1.checked == true) {
-	wrap[0].style.left = "50px";
-} else if (document.getElementById("slide-1-trigger").checked == true) {
-} else if (document.getElementById("slide-1-trigger").checked == true) {
-} else if (document.getElementById("slide-1-trigger").checked == true) {
+// var wrap = document.getElementsByClassName("wrap");
+// var slide1 = document.getElementById("slide-1-trigger");
+// var input = document.getElementById("projHeader");
+// console.dir(input);
+// // console.dir(document.getElementById("slide-1-trigger").checked);
+// // console.log(slide11);
+// console.log(wrap[0]);
+// if (slide1.checked == true) {
+// 	wrap[0].style.left = "50px";
+// 	alert("yay!");
+if (document.getElementById("slide-2-trigger").checked == true) {
+	alert("yay");
+} else if (document.getElementById("slide-3-trigger").checked == true) {
+} else if (document.getElementById("slide-4-trigger").checked == true) {
 }
